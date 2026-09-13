602e983f61d1e0bc114b41f8e004a1f3dd59440f
kushagrasingh0100 <kushagrasinghrajput973@gmail.com>
2026-09-13 23:11:04 +0530

    Add even or odd checker in 02_even_or_odd.c

---
+++ 02_even_or_odd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n = 10;
+
+    if (n % 2 == 0) {
+        printf("%d is Even", n);
+    } else {
+        printf("%d is Odd", n);
+    }
+
+    return 0;
+}
